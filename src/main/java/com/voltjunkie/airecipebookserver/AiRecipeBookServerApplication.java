de6071c2efd008f3acf3a75d788b8b9b161696ae
ruslan ishmaev <ruslanishmaev03@gmail.com>
2024-04-18 01:32:52 +0300

    initial commit

---
+++ src/main/java/com/voltjunkie/airecipebookserver/AiRecipeBookServerApplication.java
@@ -0,0 +1,13 @@
+package com.voltjunkie.airecipebookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecipeBookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRecipeBookServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/voltjunkie/airecipebookserver/AiRecipeBookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voltjunkie.airecipebookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRecipeBookServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
